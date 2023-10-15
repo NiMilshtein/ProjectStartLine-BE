d8a487049850d54feaee6753ad6d10ed91933015
Nimi <nicefull43@gmail.com>
2023-10-15 11:59:00 +0300

    remove libary

--- routes/pixabayRoute.js
+++ routes/pixabayRoute.js
@@ -1,5 +1,5 @@
+const https = require('https');
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
 
 const apiKey = '38025714-8dab2bc0a63c5f98c1f2b7f87';
@@ -7,30 +7,49 @@ const apiKey = '38025714-8dab2bc0a63c5f98c1f2b7f87';
 router.get('/', async (req, res) => {
   const { q, page } = req.query;
 
-  try {
-    const { data } = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: apiKey,
-        q,
-        lang: 'en',
-        image_type: 'photo',
-        orientation: 'all',
-        category: 'nature',
-        min_width: 100,
-        min_height: 100,
-        editors_choice: false,
-        safesearch: true,
-        order: 'popular',
-        page,
-        per_page: 20,
-      },
-    });
+  const queryParams = new URLSearchParams({
+    key: apiKey,
+    q,
+    lang: 'en',
+    image_type: 'photo',
+    orientation: 'all',
+    category: 'nature',
+    min_width: 100,
+    min_height: 100,
+    editors_choice: false,
+    safesearch: true,
+    order: 'popular',
+    page,
+    per_page: 20,
+  });
 
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching data from Pixabay:', error);
-    res.status(500).json({ message: 'Error fetching data from Pixabay' });
-  }
+  const apiUrl = `https://pixabay.com/api/?${queryParams}`;
+
+  https.get(apiUrl, httpResponse => {
+    const { statusCode } = httpResponse;
+
+    if (statusCode !== 200) {
+      console.error(`HTTP error! Status: ${statusCode}`);
+      httpResponse.resume();
+      return res.status(500).json({ message: 'Error fetching data from Pixabay' });
+    }
+
+    let rawJsonData = '';
+    httpResponse.setEncoding('utf8');
+    httpResponse.on('data', chunk => rawJsonData += chunk);
+    httpResponse.on('end', () => {
+      try {
+        console.log(rawJsonData);
+        const parsedJsonData = JSON.parse(rawJsonData);
+        console.log(parsedJsonData);
+        res.json(parsedJsonData);
+      } catch (err) {
+        res.status(500).json({ errMessage: err.message, errStack: err.stack });
+      }
+    });      
+  }).on('error', err => {
+    res.status(500).json({ errMessage: err.message });
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express= require("express")
-const cors= require("cors");
+const express= require("express");
+const cors = require("cors");
 const pixabayRoute = require("./routes/pixabayRoute");
 const app = express();
-require("dotenv").config()
-app.use(express.json())
-app.use(express.static('images'))
-app.use(cors());
-app.use("/api/pixabay", pixabayRoute);
-// app.use("*",(req,res)=>{
-    const router = express.Router();
-//     res.status(418).send({message: "oops page not found"})
-// })
 
-app.listen(process.env.PORT||3000,()=>{
+app.use(cors());
+app.use(express.json());
+app.use(express.static('images'));
+
+app.use("/api/pixabay", pixabayRoute);
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running on port 3000")
-})
+});
